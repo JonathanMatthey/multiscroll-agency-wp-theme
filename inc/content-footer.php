4e231bfa327dbe76e314e77300f3b68b28b94b99
Jonathan Matthey <mattheyj@gmail.com>
2014-10-18 23:43:38 -0400

    footer mbile first

--- inc/content-footer.php
+++ inc/content-footer.php
@@ -6,48 +6,36 @@ if( get_option('slideout_shortcode') )
 <footer class="container master-footer">
 	<div class="row master-footer-content">
 		<div class="col-md-8 no-gutter col-md-offset-2">
-			<div class="col-xs-12 col-xl-4">
+			<div class="col-xs-12 col-xl-4 footer-logo p-m">
 				<div class="red-t">
 				</div>
 				<div class="taylor-logo-white">
 				</div>
 			</div>
 			<div class="col-xs-12 col-xl-8 no-gutter">
-				<div class="footer-links-wrapper">
-					<div class="contact-us">
-						<a href="#">Contact Us</a>
-						<div class="social-links">
-							<a class="twitter"><i class="fa fa-twitter"></i></a>
-							<a class="facebook"><i class="fa fa-facebook"></i></a>
-							<a class="linkedin"><i class="fa fa-linkedin"></i></a>
-						</div>
+				<div class="col-xs-12 col-md-3 col-lg-2 col-lg-offset-1 p-m">
+					<a class="footer-link" href="#">Agency</a>
+					<a class="footer-link" href="#">People</a>
+				</div>
+				<div class="col-xs-12 col-md-3 col-lg-2 p-m">
+					<a class="footer-link" href="#">Work</a>
+					<a class="footer-link" href="#">Clients</a>
+				</div>
+				<div class="col-xs-12 col-md-3 col-lg-2 p-m">
+					<a class="footer-link" href="#">Capabilities</a>
+					<a class="footer-link" href="#">Awards</a>
+				</div>
+				<div class="col-xs-12 col-md-3 col-lg-2 p-m">
+					<a class="footer-link" href="#">Opportunities</a>
+					<a class="footer-link" href="#">News</a>
+				</div>
+				<div class="col-xs-12 col-md-12 col-lg-2 p-m contact-us">
+					<a class="footer-link" href="#">Contact Us</a>
+					<div class="social-links">
+						<a class="linkedin"><i class="fa fa-linkedin"></i></a>
+						<a class="facebook"><i class="fa fa-facebook"></i></a>
+						<a class="twitter"><i class="fa fa-twitter"></i></a>
 					</div>
-					<ul>
-						<li>
-							<a href="#">Agency</a>
-						</li>
-						<li>
-							<a href="#">People</a>
-						</li>
-						<li>
-							<a href="#">Work</a>
-						</li>
-						<li>
-							<a href="#">Clients</a>
-						</li>
-						<li>
-							<a href="#">Capabilities</a>
-						</li>
-						<li>
-							<a href="#">Awards</a>
-						</li>
-						<li>
-							<a href="#">Opportunities</a>
-						</li>
-						<li>
-							<a href="#">News</a>
-						</li>
-					</ul>
 				</div>
 			</div>
 		</div>
