1e4a77b5998e49f4a33dd9072a27bd4a87b9a53f
Jonathan Matthey <mattheyj@gmail.com>
2014-10-18 23:56:28 -0400

    footer- done !

--- inc/content-footer.php
+++ inc/content-footer.php
@@ -13,28 +13,28 @@ if( get_option('slideout_shortcode') )
 				</div>
 			</div>
 			<div class="col-xs-12 col-xl-8 no-gutter">
-				<div class="col-xs-12 col-md-3 col-lg-2 col-lg-offset-1 p-m">
+				<div class="col-xs-12 col-sm-2 col-sm-offset-2 col-md-2 col-md-offset-1 col-lg-2 col-lg-offset-1 p-m">
 					<a class="footer-link" href="#">Agency</a>
 					<a class="footer-link" href="#">People</a>
 				</div>
-				<div class="col-xs-12 col-md-3 col-lg-2 p-m">
+				<div class="col-xs-12 col-sm-2 col-md-2 col-lg-2 p-m">
 					<a class="footer-link" href="#">Work</a>
 					<a class="footer-link" href="#">Clients</a>
 				</div>
-				<div class="col-xs-12 col-md-3 col-lg-2 p-m">
+				<div class="col-xs-12 col-sm-2 col-md-2 col-lg-2 p-m">
 					<a class="footer-link" href="#">Capabilities</a>
 					<a class="footer-link" href="#">Awards</a>
 				</div>
-				<div class="col-xs-12 col-md-3 col-lg-2 p-m">
+				<div class="col-xs-12 col-sm-2 col-md-2 col-lg-2 p-m">
 					<a class="footer-link" href="#">Opportunities</a>
 					<a class="footer-link" href="#">News</a>
 				</div>
-				<div class="col-xs-12 col-md-12 col-lg-2 p-m contact-us">
+				<div class="col-xs-12 col-md-12 col-md-2 col-lg-2 p-m contact-us">
 					<a class="footer-link" href="#">Contact Us</a>
 					<div class="social-links">
-						<a class="linkedin"><i class="fa fa-linkedin"></i></a>
-						<a class="facebook"><i class="fa fa-facebook"></i></a>
 						<a class="twitter"><i class="fa fa-twitter"></i></a>
+						<a class="facebook"><i class="fa fa-facebook"></i></a>
+						<a class="linkedin"><i class="fa fa-linkedin"></i></a>
 					</div>
 				</div>
 			</div>
