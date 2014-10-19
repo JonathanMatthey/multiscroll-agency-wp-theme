4f4af9e649a98b8148d25f517cbe659b08166136
Jonathan Matthey <mattheyj@gmail.com>
2014-10-18 23:17:02 -0400

    footer content

--- inc/content-footer.php
+++ inc/content-footer.php
@@ -1,48 +1,63 @@
 <?php
-	if( get_option('slideout_shortcode') )
-		echo do_shortcode( get_option('slideout_shortcode') ); 
+if( get_option('slideout_shortcode') )
+	echo do_shortcode( get_option('slideout_shortcode') );
 ?>
 
 <footer class="container master-footer">
-	<div class="row">
-		
-		<?php
-			if( is_active_sidebar('footer1') && !( is_active_sidebar('footer2') ) && !( is_active_sidebar('footer3') ) && !( is_active_sidebar('footer4') ) ){
-				echo '<div class="col-md-12 footer-column white-bg"><div class="valign">';
-				dynamic_sidebar('footer1');
-				echo '</div></div>';
-			}
-				
-			if( is_active_sidebar('footer2') && !( is_active_sidebar('footer3') ) && !( is_active_sidebar('footer4') ) ){
-				echo '<div class="col-md-6 footer-column white-bg"><div class="valign">';
-					dynamic_sidebar('footer1');
-				echo '</div></div><div class="col-md-6 footer-column black-bg"><div class="valign">';
-					dynamic_sidebar('footer2');
-				echo '</div></div><div class="clear"></div>';
-			}
-				
-			if( is_active_sidebar('footer3') && !( is_active_sidebar('footer4') ) ){
-				echo '<div class="col-md-4 footer-column white-bg"><div class="valign">';
-					dynamic_sidebar('footer1');
-				echo '</div></div><div class="col-md-4 footer-column black-bg"><div class="valign">';
-					dynamic_sidebar('footer2');
-				echo '</div></div><div class="col-md-4 footer-column white-bg"><div class="valign">';
-					dynamic_sidebar('footer3');
-				echo '</div></div><div class="clear"></div>';
-			}
-			
-			if( ( is_active_sidebar('footer4') ) ){
-				echo '<div class="col-md-3 footer-column white-bg"><div class="valign">';
-					dynamic_sidebar('footer1');
-				echo '</div></div><div class="col-md-3 footer-column black-bg"><div class="valign">';
-					dynamic_sidebar('footer2');
-				echo '</div></div><div class="col-md-3 footer-column white-bg"><div class="valign">';
-					dynamic_sidebar('footer3');
-				echo '</div></div><div class="col-md-3 footer-column black-bg"><div class="valign">';
-					dynamic_sidebar('footer4');
-				echo '</div></div><div class="clear"></div>';
-			}
-		?>
-
+	<div class="row master-footer-content">
+		<div class="col-md-8 no-gutter col-md-offset-2">
+			<div class="col-xs-12 col-xl-4">
+				<div class="red-t">
+				</div>
+				<div class="taylor-logo-white">
+				</div>
+			</div>
+			<div class="col-xs-12 col-xl-8 no-gutter">
+				<div class="footer-links-wrapper">
+					<div class="contact-us">
+						<a href="#">Contact Us</a>
+						<div class="social-links">
+							<a class="twitter"><i class="fa fa-twitter"></i></a>
+							<a class="facebook"><i class="fa fa-facebook"></i></a>
+							<a class="linkedin"><i class="fa fa-linkedin"></i></a>
+						</div>
+					</div>
+					<ul>
+						<li>
+							<a href="#">Agency</a>
+						</li>
+						<li>
+							<a href="#">People</a>
+						</li>
+						<li>
+							<a href="#">Work</a>
+						</li>
+						<li>
+							<a href="#">Clients</a>
+						</li>
+						<li>
+							<a href="#">Capabilities</a>
+						</li>
+						<li>
+							<a href="#">Awards</a>
+						</li>
+						<li>
+							<a href="#">Opportunities</a>
+						</li>
+						<li>
+							<a href="#">News</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row sub-footer">
+		<div class="col-md-8 col-md-offset-2">
+			Copyright @ 2011 - 2014 Taylor Strategy
+			<span class="made-in-nyc">
+				Made in NYC
+			</span>
+		</div>
 	</div>
 </footer>
