dc03526a34eaa448cb5d6fe46eb2a56520a4c319
Jonathan Matthey <mattheyj@gmail.com>
2014-10-20 12:03:17 -0400

    shrink footer logo

--- inc/content-footer.php
+++ inc/content-footer.php
@@ -5,15 +5,15 @@ if( get_option('slideout_shortcode') )
 
 <footer class="container master-footer">
 	<div class="row master-footer-content">
-		<div class="col-md-8 no-gutter col-md-offset-2">
-			<div class="col-xs-12 col-xl-4 footer-logo p-m">
+		<div class="col-md-8 col-lg-10 col-lg-offset-1 no-gutter col-md-offset-2">
+			<div class="col-xs-12 col-lg-2 col-xl-4 footer-logo p-m">
 				<div class="red-t">
 				</div>
 				<div class="taylor-logo-white">
 				</div>
 			</div>
-			<div class="col-xs-12 col-xl-8 no-gutter">
-				<div class="col-xs-12 col-sm-2 col-sm-offset-2 col-md-2 col-md-offset-1 col-lg-2 col-lg-offset-1 p-m">
+			<div class="col-xs-12 col-lg-10 col-xl-8 no-gutter">
+				<div class="col-xs-12 col-sm-2 col-sm-offset-2 col-md-2 col-md-offset-1 col-lg-2 col-lg-offset-2 p-m">
 					<a class="footer-link" href="#">Agency</a>
 					<a class="footer-link" href="#">People</a>
 				</div>
