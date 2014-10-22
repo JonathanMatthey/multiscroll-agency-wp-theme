3c693088d55a36c02330dcbe68ed3a9f26889584
Jonathan Matthey <mattheyj@gmail.com>
2014-10-22 12:09:05 -0400

    teampages

--- loop/content-team-right.php
+++ loop/content-team-right.php
@@ -1,24 +1,24 @@
-<?php 
+<?php
 	$job = get_post_meta( $post->ID, '_ebor_the_job_title', true );
 ?>
 
-<article class="col-md-3 team-info white-bg text-center">
+<article class="col-md-2 no-gutter team-info white-bg text-center">
 	<div class="triangle-arrow triangle-arrow-left"></div>
-	
+
 	<div class="valign">
-		<?php 
+		<?php
 			the_title('<h3 class="sub-heading dark"><a href="'. get_permalink() .'">', '</a></h3>');
-			
+
 			if( $job )
 				echo '<h6><span>'. $job .'</span></h6>';
-				
+
 			the_excerpt();
 			get_template_part('inc/content','team-social');
 		?>
 	</div>
-	
+
 </article>
 
-<article class="col-md-3 no-gutter team-thumb silver-bg text-center">
+<article class="col-md-2 no-gutter team-thumb silver-bg text-center">
 	<?php the_post_thumbnail('large', array('class' => 'img-responsive')); ?>
 </article>
